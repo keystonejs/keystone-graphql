425f129a62dae16881d77fa00e7273f77fd9e27f
vitalbone <bz.vito@gmail.com>
2016-01-25 18:12:54 +1100

    Adding encryption to Password type

--- types/Password.js
+++ types/Password.js
@@ -5,11 +5,17 @@ const GraphQL = require('graphql');
 module.exports = (field) => ({
 	type: GraphQL.GraphQLString,
 	args: {
-		workFactor: {
-			type: GraphQL.GraphQLInt,
-			defaultValue: 10,
-			description: 'The bcrypt workfactor to use when generating the hash',
+		character: {
+			type: GraphQL.GraphQLString,
+			defaultValue: '•',
+			description: 'The character to use to represent a set password value',
 		},
 	},
-	resolve: (source, args) => source.get(field.path),
+	resolve: (source, args) => {
+		if (!source.get(field.path)) return '';
+		var len = Math.round(Math.random() * 4) + 6;
+		var chrs = '';
+		for (var i = 0; i < len; i++) chrs += args.character;
+		return chrs;
+	}
 });

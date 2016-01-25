ffcdf2002e0595c6fff05cd85f9606578b8a959b
vitalbone <bz.vito@gmail.com>
2016-01-25 15:21:17 +1100

    Amendments to Keystone Code type

--- types/Code.js
+++ types/Code.js
@@ -2,29 +2,23 @@
 
 var GraphQL = require('graphql');
 
-var KeystoneCodeType = new GraphQL.GraphQLObjectType({
-	name: 'KeystoneCode',
-	fields: {
-		code: {
+module.exports = (field) => ({
+	type: GraphQL.GraphQLString,
+	args: {
+		size: {
 			type: GraphQL.GraphQLString,
-			args: {
-				size: {
-					type: GraphQL.GraphQLString,
-					defaultValue: 'full',
-					description: 'The size of the text area',
-				},
-				height: {
-					type: GraphQL.GraphQLInt,
-					defaultValue: 180,
-					description: 'The height of the text area',
-				},
-				lang: {
-					type: GraphQL.GraphQLString,
-					description: 'The config property',
-				},
-			},
+			defaultValue: 'full',
+			description: 'The size of the text area',
+		},
+		height: {
+			type: GraphQL.GraphQLInt,
+			defaultValue: 180,
+			description: 'The height of the text area',
+		},
+		lang: {
+			type: GraphQL.GraphQLString,
+			description: 'The config property',
 		},
 	},
+	resolve: (source, args) => source.get(field.path),
 });
-
-module.exports = (field) => KeystoneCodeType;

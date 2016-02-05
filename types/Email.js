58579da59afdcb27b0efb2751f6134dad17a5b04
vitalbone <bz.vito@gmail.com>
2016-02-05 17:19:49 +1100

    Removing unnecessary args in Email field type

--- types/Email.js
+++ types/Email.js
@@ -4,18 +4,5 @@ const GraphQL = require('graphql');
 
 module.exports = (field) => ({
 	type: GraphQL.GraphQLString,
-	args: {
-		validInput: {
-			type: GraphQL.GraphQLString,
-			description: 'Validates that a valid email has been provided.'
-		},
-	},
-	resolve: (source, args, required, data) => {
-		var value = this.getValueFromData(data);
-		if (value) {
-			return utils.isEmail(value);
-		} else {
-			return (!required || (value !== undefined && source && source.get(this.path))) ? true : false;
-		}
-	},
+	resolve: (source) => source.get(field.path),
 });

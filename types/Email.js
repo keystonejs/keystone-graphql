4de590f0fb738f3aedb98684c620c8cf4e9973e1
vitalbone <bz.vito@gmail.com>
2016-02-05 11:56:24 +1100

    Removing gravatrUrl from Email field type

--- types/Email.js
+++ types/Email.js
@@ -2,34 +2,20 @@
 
 const GraphQL = require('graphql');
 
-const KeystoneEmailType = new GraphQL.GraphQLObjectType({
-	name: 'KeystoneEmail',
-	fields: {
-		email: { type: GraphQL.GraphQLString },
-		gravatarUrl: {
+module.exports = (field) => ({
+	type: GraphQL.GraphQLString,
+	args: {
+		validInput: {
 			type: GraphQL.GraphQLString,
-			args: {
-				size: {
-					type: GraphQL.GraphQLInt,
-					defaultValue: 80,
-					description: 'Size of images ranging from 1 to 2048 pixels, square',
-				},
-				defaultImage: {
-					type: GraphQL.GraphQLString,
-					defaultValue: 'identicon',
-					description: 'Default image url encoded href or one of the built in options: 404, mm, identicon, monsterid, wavatar, retro, blank',
-				},
-				rating: {
-					type: GraphQL.GraphQLString,
-					defaultValue: 'g',
-					description: 'The rating of the image, either: g, pg, r or x',
-				},
-			},
-			description: 'Protocol-less Gravatar image request URL',
-			resolve: (source, args) =>
-				source.gravatarUrl(args.size, args.defaultImage, args.rating)
+			description: 'Validates that a valid email has been provided.'
 		},
 	},
+	resolve: (source, args, required, data) => {
+		var value = this.getValueFromData(data);
+		if (value) {
+			return utils.isEmail(value);
+		} else {
+			return (!required || (value !== undefined && source && source.get(this.path))) ? true : false;
+		}
+	},
 });
-
-module.exports = (field) => KeystoneEmailType;

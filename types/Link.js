069d340c43542b680673e3b18ffdcb36a22f9573
vitalbone <bz.vito@gmail.com>
2016-02-05 17:19:56 +1100

    Removing unnecessary args in Link field type

--- types/Link.js
+++ types/Link.js
@@ -2,18 +2,7 @@
 
 var GraphQL = require('graphql');
 
-var KeystoneLinkType = new GraphQL.GraphQLObjectType({
-	name: 'Keystone Link',
-	fields: {
-		raw: {
-			type: GraphQL.GraphQLString,
-			description: 'The raw unformatted URL',
-		},
-		format: {
-			type: GraphQL.GraphQLString,
-			description: 'The URL after being passed through the `format Function` option',
-		},
-	},
-});
-
-module.exports = (field) => KeystoneLinkType;
+module.exports = (field) => ({
+	type: GraphQL.GraphQLString,
+	resolve: (source) => source.get(field.path),
+})

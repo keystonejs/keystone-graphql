bf9a20612128d7f2ab10a7041277af30f5d9cdeb
vitalbone <bz.vito@gmail.com>
2016-02-05 17:19:41 +1100

    Removing unnecessary args in Code field type

--- types/Code.js
+++ types/Code.js
@@ -4,21 +4,5 @@ const GraphQL = require('graphql');
 
 module.exports = (field) => ({
 	type: GraphQL.GraphQLString,
-	args: {
-		size: {
-			type: GraphQL.GraphQLString,
-			defaultValue: 'full',
-			description: 'The size of the text area',
-		},
-		height: {
-			type: GraphQL.GraphQLInt,
-			defaultValue: 180,
-			description: 'The height of the text area',
-		},
-		lang: {
-			type: GraphQL.GraphQLString,
-			description: 'The config property',
-		},
-	},
-	resolve: (source, args) => source.get(field.path),
+	resolve: (source) => source.get(field.path),
 });

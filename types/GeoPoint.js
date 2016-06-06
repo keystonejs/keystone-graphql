0bf674ecd935108a678933c18d0efa7da9c893df
Brian Doyle <brian@tf83.com>
2016-06-06 12:01:49 -0700

    Added new Keystone Types:
    - Color
    - GeoPoint
    - S3File

---
+++ types/GeoPoint.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const GraphQL = require('graphql');
+
+module.exports = (field) => ({
+	type: GraphQL.GraphQLList(GraphQL.GraphQLString),
+  description: 'An array [longitude, latitude]',
+	resolve: (source) => source.get(field.path),
+});

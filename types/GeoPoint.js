bbe110de1b137f79582e12f8fda93ad1ff6df7c5
Jed Watson <jed.watson@me.com>
2016-06-08 22:48:34 +1000

    Fixing code style issues

--- types/GeoPoint.js
+++ types/GeoPoint.js
@@ -4,6 +4,6 @@ const GraphQL = require('graphql');
 
 module.exports = (field) => ({
 	type: GraphQL.GraphQLList(GraphQL.GraphQLString),
-  description: 'An array [longitude, latitude]',
+	description: 'An array [longitude, latitude]',
 	resolve: (source) => source.get(field.path),
 });
--- types/S3File.js
+++ types/S3File.js
@@ -5,13 +5,13 @@ const GraphQL = require('graphql');
 const S3FileType = new GraphQL.GraphQLObjectType({
 	name: 'KeystoneS3FileType',
 	fields: {
-    filename: { type: GraphQL.GraphQLString },
-    originalname: { type: GraphQL.GraphQLString },
-    path: { type: GraphQL.GraphQLString },
-    size: { type: GraphQL.GraphQLInt },
-    filetype: { type: GraphQL.GraphQLString },
-    url: { type: GraphQL.GraphQLString }
-  }
+		filename: { type: GraphQL.GraphQLString },
+		originalname: { type: GraphQL.GraphQLString },
+		path: { type: GraphQL.GraphQLString },
+		size: { type: GraphQL.GraphQLInt },
+		filetype: { type: GraphQL.GraphQLString },
+		url: { type: GraphQL.GraphQLString }
+	}
 });
 
 module.exports = (field) => S3FileType;
